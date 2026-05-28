b2e9c72d59dd3d402eb73e39b9e0d6d0b486887c
Satsbe09 <satyamsingh4921@gmail.com>
2026-05-28 15:21:30 +0530

    logic building start

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import express from "express";
-import cros from "cors";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app =express();
-app.use(cros({
+app.use(cors({
     origin:process.env.cros_origin,
     credentials:true
 }));
@@ -11,4 +11,10 @@ app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true,limit:"16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+
+// route import
+ import userRouter from './routes/user.route.js';
+ //routes declaration
+ app.use("/api/v1/users",userRouter)
 export {app};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
  //require ('dotenv').config({path:"./env"});
  import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
-import express from 'express';
+import { app } from "./app.js";
+
 
 dotenv.config({path:"./.env"});
-
-const app = express();
 
 connectDB().then(() => {
     app.listen(process.env.PORT || 8000, () => {
